b265d6d8c36575e97c191c731cef13fee90c4ab0
Aytgg <aytugdeger.2003@gmail.com>
2024-10-22 09:05:15 +0300

    Deleted unnecessary things.

--- resources/views/fon.blade.php
+++ resources/views/fon.blade.php
@@ -60,49 +60,6 @@
                 href="/fon/{{ $fon->code }}/rakip-analizi">Rakip Analizi</a>
         </nav>
 
-
-        {{-- <div class="row ml-1 my-3">
-            <div class="col-sm-6">
-                <div class="btn-group btn-group-toggle float-left" data-toggle="buttons">
-                    <label class="btn btn-sm btn-primary btn-simple active" id="0">
-                        <input id="radio0" type="radio" name="options" checked>
-                        <span class="d-none d-sm-block d-md-block d-lg-block d-xl-block">7G</span>
-                        <span class="d-block d-sm-none">
-                            <i class="tim-icons icon-single-02"></i>
-                        </span>
-                    </label>
-                    <label class="btn btn-sm btn-primary btn-simple" id="1">
-                        <input id="radio1" type="radio" class="d-none d-sm-none" name="options">
-                        <span class="d-none d-sm-block d-md-block d-lg-block d-xl-block">1A</span>
-                        <span class="d-block d-sm-none">
-                            <i class="tim-icons icon-gift-2"></i>
-                        </span>
-                    </label>
-                    <label class="btn btn-sm btn-primary btn-simple" id="2">
-                        <input id="radio2" type="radio" class="d-none" name="options">
-                        <span class="d-none d-sm-block d-md-block d-lg-block d-xl-block">3A</span>
-                        <span class="d-block d-sm-none">
-                            <i class="tim-icons icon-tap-02"></i>
-                        </span>
-                    </label>
-                    <label class="btn btn-sm btn-primary btn-simple" id="3">
-                        <input id="radio3" type="radio" class="d-none" name="options">
-                        <span class="d-none d-sm-block d-md-block d-lg-block d-xl-block">1Y</span>
-                        <span class="d-block d-sm-none">
-                            <i class="tim-icons icon-tap-02"></i>
-                        </span>
-                    </label>
-                    <label class="btn btn-sm btn-primary btn-simple" id="4">
-                        <input id="radio4" type="radio" class="d-none" name="options">
-                        <span class="d-none d-sm-block d-md-block d-lg-block d-xl-block">3Y</span>
-                        <span class="d-block d-sm-none">
-                            <i class="tim-icons icon-tap-02"></i>
-                        </span>
-                    </label>
-                </div>
-            </div>
-        </div> --}}
-
         <!-- Fon Price Period Buttons -->
         <div class="row ml-1 my-3">
             <div class="col-sm-6">
